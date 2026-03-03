ae3e0eca00204adaa457fe6fd87c5ebe2a5a8eca
lizzaaabb <lbogveradze12@gmail.com>
2026-03-03 19:22:33 +0400

    useclient

--- src/app/components/Templates.js
+++ src/app/components/Templates.js
@@ -1,3 +1,4 @@
+'use client'
 import React, { useEffect, useRef, useState } from 'react'
 import '../styles/Templates.css'
 import TemplateHeader from './TemplateHeader'
--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Brands from './components/Brands'
 import Projects from './components/Projects'
 import Pricing from './components/Pricing'
 import Cta from './components/Cta'
+import Templates from './components/Templates'
 
 export default function Home() {
   return (
