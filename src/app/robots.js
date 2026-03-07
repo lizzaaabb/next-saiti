7d5c06f1f522f6cbe66184068d174dd178f9737c
lizzaaabb <lbogveradze12@gmail.com>
2026-03-07 20:21:20 +0400

    sitemap added

---
+++ src/app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://saitisackoba.com/sitemap.xml',
+  }
+}
---
+++ src/app/sitemap.js
@@ -0,0 +1,34 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://saitisackoba.com',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1.0,
+    },
+    {
+      url: 'https://saitisackoba.com/saitis-damzadeba-proeqtebi',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://saitisackoba.com/saitis-damzadeba-fasebi',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://saitisackoba.com/saitis-ackoba-kontaqti',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://saitisackoba.com/templates',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+  ]
+}
